7834059870bf82ce0d85b4586062608fd88a84eb
Mutasim fuad Rafi <165637733+mfbhai10@users.noreply.github.com>
2025-01-17 14:04:06 +0000

    switch case

---
+++ 04_control_flow/second.js
@@ -0,0 +1,17 @@
+const month = "jan"
+
+switch(month){
+    case "jan":
+        console.log("January");
+        break;     
+    case "feb":
+        console.log("February");
+        break;     
+    case "march":
+        console.log("March");
+        break;   
+    
+    default:
+        console.log("Invalid");
+        break;
+}
